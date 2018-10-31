45196e4b531a633bbec0099030016a1982e5030b
kogerr <kogerr@hotmail.com>
2018-10-31 15:14:23 +0100

    make header configurable

--- web/src/app/header/header.component.ts
+++ web/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -7,11 +7,38 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Input() slots: number;
+  @Input() gapRatio: number;
+  gapSize: number;
+  slotSize: number;
+  columns: string;
+
+  menuItems = [
+    {path: 'calendar', image: 'calendar-icon.svg'},
+    {path: 'statistics', image: 'statistics-icon.svg'},
+    {path: 'add', image: 'plus-icon.svg'}
+  ];
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    console.log(this.route);
+    this.calculateSizes();
+  }
+
+  calculateSizes(): void {
+    console.log(this.slots, this.gapRatio);
+    console.log(this.slots * (+this.gapRatio + 1) + 1);
+    this.gapSize = 100 / (this.slots * (+this.gapRatio + 1) + 1);
+    this.slotSize = this.gapSize * this.gapRatio;
+    this.buildColumns();
+  }
+
+  buildColumns(): void {
+    let columns = '';
+    for (let i = 0; i < this.slots; i++) {
+      columns += this.slotSize + 'vw ';
+    }
+    this.columns = columns.trimRight();
   }
 
 }
